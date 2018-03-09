894f6b47a0fd55dba79451b9fd4dd4dff3ad511d
Orange Lee <lycheng1215@sina.com>
2018-03-09 17:44:35 +0800

    2018-3-8
    T1

--- Source Code/李沿橙 2018-3-8/source/李沿橙/fst.cpp
+++ Source Code/李沿橙 2018-3-8/source/李沿橙/fst.cpp
@@ -64,50 +64,23 @@ void printOut(INT_PUT x)
 
 const int maxn = 30005;
 int n, r, k;
-LL a[3][maxn];
+LL a[maxn], b[maxn], c[maxn];
 
 #define RunInstance(x) delete new x
-struct brute
-{
-	static const int maxN = 505;
-	LL sum[maxN * maxN];
-
-	brute() : sum()
-	{
-		int to = n - r + 1;
-		for (int x = 1; x <= to; x++)
-		{
-			for (int y = x + 1; y <= to; y++)
-			{
-				LL& ans = sum[++sum[0]];
-				ans = 0;
-				for (int i = 1; i <= n; i++)
-				{
-					int cnt = 0;
-					if (x <= i && i <= x + r - 1) cnt++;
-					if (y <= i && i <= y + r - 1) cnt++;
-					ans += a[cnt][i];
-				}
-			}
-		}
-		std::nth_element(sum + 1, sum + k, sum + 1 + sum[0]);
-		printOut(sum[k]);
-	}
-};
 struct work
 {
-	int bound;
-	
 	struct BIT
 	{
-		int bit[maxn * 2];
+		int bound;
+		int bit[maxn];
+		BIT() : bit() {}
 		static inline int lowbit(int x)
 		{
 			return x & -x;
 		}
 		void add(int pos, int val)
 		{
-			while (pos <= bound)
+			while (pos < bound)
 			{
 				bit[pos] += val;
 				pos += lowbit(pos);
@@ -116,7 +89,7 @@ struct work
 		int query(int pos)
 		{
 			int ret = 0;
-			while (ret)
+			while (pos)
 			{
 				ret += bit[pos];
 				pos ^= lowbit(pos);
@@ -125,42 +98,101 @@ struct work
 		}
 		int query(int l, int r)
 		{
+			if (l == r + 1) return 0;
+			if (!l) return query(r);
 			return query(r) - query(l - 1);
 		}
-	} bit;
-	
-
-	static bool check(LL s)
-	{
-		LL sum1 = 0;
-		for (int i = 1; i <= r; i++)
+		void clear(int bound)
 		{
-			
+			memset(bit, 0, sizeof(int) * (bound + 1));
+			this->bound = bound;
 		}
+	} bit;
+	LL disc1[maxn];
+	LL disc2[maxn];
+	void discretize()
+	{
+		std::sort(disc1 + 1, disc1 + 1 + disc1[0]);
+		disc1[0] = std::unique(disc1 + 1, disc1 + 1 + disc1[0]) - (disc1 + 1);
+		std::sort(disc2 + 1, disc2 + 1 + disc2[0]);
+		disc2[0] = std::unique(disc2 + 1, disc2 + 1 + disc2[0]) - (disc2 + 1);
+	}
+	LL calc1(int j)
+	{
+		return a[j - 1] + b[j + r - 1] - b[j - 1] - a[j + r - 1];
+	}
+	LL calc2(int j)
+	{
+		return b[j - 1] - c[j - 1] + b[j + r - 1] - a[j + r - 1];
+	}
+	int code1(LL val)
+	{
+		return std::upper_bound(disc1 + 1, disc1 + 1 + disc1[0], val) - 1 - disc1;
+	}
+	int code2(LL val)
+	{
+		return std::upper_bound(disc2 + 1, disc2 + 1 + disc2[0], val) - 1 - disc2;
+	}
+	LL v1[maxn];
+	LL v2[maxn];
+	int dis1[maxn];
+	int dis2[maxn];
+	LL sub1[maxn];
+	LL sub2[maxn];
+	LL check(LL s)
+	{
+		LL ret = 0;
+		bit.clear(disc1[0] + 10);
+		for (int i = n - r - r + 1; i >= 1; i--)
+		{
+			bit.add(dis1[i + r], 1);
+			ret += bit.query(code1(s - sub1[i]));
+		}
+		bit.clear(disc2[0] + 10);
+		for (int i = 2, to = std::min(r, n - r + 1); i <= to; i++)
+			bit.add(dis2[i], 1);
+		for (int i = 1; i < n - r + 1; i++)
+		{
+			ret += bit.query(code2(s - sub2[i]));
+			if (i + r <= n - r + 1) bit.add(dis2[i + r], 1);
+			bit.add(dis2[i + 1], -1);
+		}
+		return ret;
 	}
 	work()
 	{
 		for (int i = 2; i <= n; i++)
 		{
-			a[0][i] += a[0][i - 1];
-			a[1][i] += a[1][i - 1];
-			a[2][i] += a[2][i - 1];
+			a[i] += a[i - 1];
+			b[i] += b[i - 1];
+			c[i] += c[i - 1];
 		}
+		disc1[0] = disc2[0] = 0;
+		for (int i = r + 1, to = n - r + 1; i <= to; i++)
+			disc1[++disc1[0]] = v1[i] = calc1(i);
+		for (int i = 2, to = n - r + 1; i <= to; i++)
+			disc2[++disc2[0]] = v2[i] = calc2(i);
+		discretize();
 
-		LL l = 0, r = 0;
-		for (int i = 1; i <= n; i++) l += a[0][i];
-		for (int i = 1; i <= n; i++) r += a[2][i];
-		r++;
+		for (int i = r + 1, to = n - r + 1; i <= to; i++)
+			dis1[i] = code1(v1[i]);
+		for (int i = 2, to = n - r + 1; i <= to; i++)
+			dis2[i] = code2(v2[i]);
+		for (int i = n - r - r + 1; i >= 1; i--)
+			sub1[i] = a[n] + (a[i - 1] + b[i + r - 1] - b[i - 1] - a[i + r - 1]);
+		for (int i = 1; i < n - r + 1; i++)
+			sub2[i] = a[n] + (a[i - 1] - b[i - 1] + c[i + r - 1] - b[i + r - 1]);
 
-		while (r - l > 1)
+		LL left = a[n], right = c[n];
+		while (right - left > 0)
 		{
-			LL mid = l + ((r - l) >> 1);
-			if (check(mid) <= k)
-				l = mid;
+			LL mid = left + ((right - left) >> 1);
+			if (check(mid) < k)
+				left = mid + 1;
 			else
-				r = mid;
+				right = mid;
 		}
-		printOut(l);
+		printOut(right);
 	}
 };
 
@@ -169,14 +201,11 @@ void run()
 	n = readIn();
 	r = readIn();
 	k = readIn();
-	for (int i = 1; i <= n; i++) a[0][i] = readIn();
-	for (int i = 1; i <= n; i++) a[1][i] = readIn();
-	for (int i = 1; i <= n; i++) a[2][i] = readIn();
+	for (int i = 1; i <= n; i++) a[i] = readIn();
+	for (int i = 1; i <= n; i++) b[i] = readIn();
+	for (int i = 1; i <= n; i++) c[i] = readIn();
 
-	if (n <= 500)
-		RunInstance(brute);
-	else
-		RunInstance(work);
+	RunInstance(work);
 }
 
 int main()
