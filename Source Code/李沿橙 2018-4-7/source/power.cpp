fb4542831254931214489e24c44674c4d4d3f5b9
Orange Lee <lycheng1215@sina.com>
2018-04-10 17:32:15 +0800

    2018-4-7
    2018-4-7 T3

--- Source Code/李沿橙 2018-4-7/source/power.cpp
+++ Source Code/李沿橙 2018-4-7/source/power.cpp
@@ -23,100 +23,117 @@ typedef unsigned long long ULL;
 using std::cin;
 using std::cout;
 using std::endl;
-typedef LL INT_PUT;
+typedef int INT_PUT;
 INT_PUT readIn()
 {
-	INT_PUT a = 0;
-	bool positive = true;
+	INT_PUT a = 0; bool positive = true;
 	char ch = getchar();
 	while (!(ch == '-' || std::isdigit(ch))) ch = getchar();
-	if (ch == '-')
-	{
-		positive = false;
-		ch = getchar();
-	}
-	while (std::isdigit(ch))
-	{
-		(a *= 10) -= ch - '0';
-		ch = getchar();
-	}
+	if (ch == '-') { positive = false; ch = getchar(); }
+	while (std::isdigit(ch)) { a = a * 10 - (ch - '0'); ch = getchar(); }
 	return positive ? -a : a;
 }
 void printOut(INT_PUT x)
 {
-	char buffer[20];
-	int length = 0;
-	if (x < 0) putchar('-');
-	else x = -x;
+	char buffer[20]; int length = 0;
+	if (x < 0) putchar('-'); else x = -x;
 	do buffer[length++] = -(x % 10) + '0'; while (x /= 10);
 	do putchar(buffer[--length]); while (length);
 	putchar('\n');
 }
 
 const int maxn = int(1e5) + 5;
-int n, m;
-int w[maxn];
-int q;
+int n;
+int m;
+int a[maxn];
 
-int gcd(int a, int b)
+const int threshold = int(1e6) + 5;
+int phi[threshold];
+bool isntPrime[threshold];
+int prime[78500];
+void init()
 {
-	return b ? gcd(b, a % b) : a;
+	phi[1] = 1;
+	isntPrime[1] = true;
+	for (int i = 2; i < threshold; i++)
+	{
+		if (!isntPrime[i])
+		{
+			prime[++prime[0]] = i;
+			phi[i] = i - 1;
+		}
+		for (int j = 1, p = prime[j], s = i * p; j <= prime[0] && s < threshold; j++, p = prime[j], s = i * p)
+		{
+			isntPrime[s] = true;
+			if (i % p)
+				phi[s] = phi[i] * (p - 1);
+			else
+			{
+				phi[s] = phi[i] * p;
+				break;
+			}
+		}
+	}
 }
-LL power(LL x, int y, int mod)
+std::map<int, int> map;
+int Phi(int x)
 {
+	if (x < threshold) return phi[x];
+	if (map.count(x)) return map[x];
+	int origin = x;
+	int ret = x;
+	int to = std::sqrt(x);
+	for (int i = 2; i <= to; i++)
+	{
+		if (x % i) continue;
+		while (!(x % i)) x /= i;
+		(ret /= i) *= i - 1;
+	}
+	if (x != 1) (ret /= x) *= x - 1;
+	return map[origin] = ret;
+}
+
+LL powerWithRulerEx(LL x, int y, int mod)
+{
+	bool xGreaterThanMod = false;
+	bool retGreaterThanMod = false;
 	LL ret = 1;
 	while (y)
 	{
-		if (y & 1) ret = ret * x % mod;
-		x = x * x % mod;
+		if (y & 1)
+		{
+			ret = ret * x;
+			if (xGreaterThanMod || ret >= mod) retGreaterThanMod = true;
+			ret %= mod;
+		}
+		x = x * x;
+		if (x >= mod) xGreaterThanMod = true;
+		x %= mod;
 		y >>= 1;
 	}
-	return ret;
+	return ret + (retGreaterThanMod ? mod : 0);
 }
 
-#define RunInstance(x) delete new x
-struct brute
+int solve(int l, int r, int mod) // 返回值可能加上了一个 mod
 {
-	static int Phi(int x)
-	{
-		int ret = x;
-		int to = std::sqrt(x);
-		for (int i = 2; i <= to; i++)
-		{
-			if (x % i) continue;
-			while (!(x % i)) x /= i;
-			(ret /= i) *= i - 1;
-		}
-		if (x != 1) (ret /= x) *= x - 1;
-		return ret;
-	}
-	LL query(int l, int r, int mod)
-	{
-		if (l == r) return w[l] % mod;
-		int phi = Phi(mod);
-		return power(w[l], query(l + 1, r, phi), mod);
-	}
-	brute()
-	{
-		while (q--)
-		{
-			int l = readIn();
-			int r = readIn();
-			printOut(query(l, r, m));
-		}
-	}
-};
+	if (l == r || mod == 1) return powerWithRulerEx(a[l], 1, mod);
+	return powerWithRulerEx(a[l], solve(l + 1, r, Phi(mod)), mod);
+}
 
 void run()
 {
-	cout << brute::Phi(10) << endl;
+	init();
 	n = readIn();
 	m = readIn();
 	for (int i = 1; i <= n; i++)
-		w[i] = readIn();
-	q = readIn();
-	if (n <= 10)
-		RunInstance(brute);
+		a[i] = readIn();
+	int q = readIn();
+	while (q--)
+	{
+		int l = readIn();
+		int r = readIn();
+		printOut(solve(l, r, m) % m);
+	}
 }
 
 int main()
--- Source Code/李沿橙 2018-4-9/source/李沿橙/lct.cpp
+++ Source Code/李沿橙 2018-4-9/source/李沿橙/lct.cpp
@@ -52,8 +52,9 @@ void printOut(INT_PUT x)
 	do putchar(buffer[--length]); while (length);
 }
 
+const LL INF = (~(LL(1) << (sizeof(LL) * 8 - 1))) >> 1;
 const int maxn = int(3e5) + 5;
-int n, k;
+int n, K;
 struct Edge
 {
 	int to;
@@ -61,11 +62,14 @@ struct Edge
 	Edge() {}
 	Edge(int to, int cost) : to(to), cost(cost) {}
 };
+std::vector<Edge> G[maxn];
+
+template<typename T, typename R>
+void maxeq(T& a, const R& b) { a = a < b ? b : a; }
 
 #define RunInstance(x) delete new x
 struct cheat
 {
-	std::vector<Edge> G[maxn];
 	LL ans;
 	LL f[maxn];
 
@@ -98,35 +102,79 @@ struct cheat
 
 	cheat() : ans()
 	{
-		for (int i = 1; i < n; i++)
-		{
-			int from = readIn();
-			int to = readIn();
-			int cost = std::abs(readIn());
-			G[from].push_back(Edge(to, cost));
-			G[to].push_back(Edge(from, cost));
-		}
-
 		DFS(1, 0);
 		printOut(ans);
 	}
 };
-struct fake : cheat
+struct brute
 {
-	fake()
+	static const int maxk = 105;
+	int size[maxn];
+	LL f[3][maxn][maxk];
+	LL non[maxk], one[maxk], two[maxk];
+	void DFS(int node, int parent)
 	{
+		size[node] = 1;
+		for (int i = 0; i < G[node].size(); i++)
+		{
+			const Edge& e = G[node][i];
+			int to = e.to;
+			if (to == parent) continue;
+			DFS(to, node);
+		}
 
+		for (int i = 0; i < G[node].size(); i++)
+		{
+			const Edge& e = G[node][i];
+			int to = e.to;
+			if (to == parent) continue;
+			LL cost = e.cost;
+
+			for (int i = 0; i <= K; i++)
+				non[i] = one[i] = two[i] = -INF; // 额外开一个大小为 k 的数组用于临时转移
+			int bound = std::min(K, size[node]); // 首先确定本次转移的最大状态量
+			for (int x = 0; x <= bound; x++)
+			{
+				int r = std::min(K - x, size[to]); // 确定子结点的最大状态量
+				for (int y = 0; y <= r; y++)
+				{
+					LL t = -INF;
+					maxeq(t, f[0][to][y]);
+					maxeq(t, f[1][to][y]);
+					maxeq(t, f[2][to][y]);
+					maxeq(non[x + y], f[0][node][x] + t);
+					maxeq(one[x + y], f[1][node][x] + t);
+					maxeq(two[x + y], f[2][node][x] + t);
+
+					maxeq(non[x + y - 1], f[0][node][x] + tf);
+				}
+			}
+		}
+	}
+
+	brute()
+	{
+		DFS(1, 0);
 	}
 };
 
 void run()
 {
 	n = readIn();
-	k = readIn();
-	if (k == 0)
+	K = readIn();
+	for (int i = 1; i < n; i++)
+	{
+		int from = readIn();
+		int to = readIn();
+		int cost = readIn();
+		G[from].push_back(Edge(to, cost));
+		G[to].push_back(Edge(from, cost));
+	}
+
+	if (K == 0)
 		RunInstance(cheat);
-	else
-		RunInstance(fake);
+	else if (K <= 100)
+		RunInstance(brute);
 }
 
 int main()
