11a57f2d4c6e1bde0d47a038b2b630a992a92676
Orange Lee <lycheng1215@sina.com>
2018-07-13 18:55:07 +0800

    2018-7-13
    2018-7-13 T1

--- Source Code/李沿橙 2018-7-13/source/李沿橙/dexterity.cpp
+++ Source Code/李沿橙 2018-7-13/source/李沿橙/dexterity.cpp
@@ -26,45 +26,31 @@ using std::endl;
 typedef LL INT_PUT;
 INT_PUT readIn()
 {
-	INT_PUT a = 0;
-	bool positive = true;
+	INT_PUT a = 0; bool positive = true;
 	char ch = getchar();
-	while (!(std::isdigit(ch) || ch == '-')) ch = getchar();
-	if (ch == '-')
-	{
-		positive = false;
-		ch = getchar();
-	}
-	while (std::isdigit(ch))
-	{
-		(a *= 10) -= ch - '0';
-		ch = getchar();
-	}
+	while (!(ch == '-' || std::isdigit(ch))) ch = getchar();
+	if (ch == '-') { positive = false; ch = getchar(); }
+	while (std::isdigit(ch)) { a = a * 10 - (ch - '0'); ch = getchar(); }
 	return positive ? -a : a;
 }
 void printOut(INT_PUT x)
 {
-	char buffer[20];
-	int length = 0;
-	if (x < 0) putchar('-');
-	else x = -x;
+	char buffer[20]; int length = 0;
+	if (x < 0) putchar('-'); else x = -x;
 	do buffer[length++] = -(x % 10) + '0'; while (x /= 10);
 	do putchar(buffer[--length]); while (length);
+	putchar('\n');
 }
 
 const int maxn = int(1e5) + 5;
 int n;
 int str[maxn * 2];
 int w[maxn], d[maxn];
-int table[3][3] =
-{
-	{ 0, 1, -1 },
-	{ -1, 0, 1 },
-	{ 1, -1, 0 }
-};
+LL sum[maxn];
+int table[3][3] = { { 0, 1, -1 },{ -1, 0, 1 },{ 1, -1, 0 } };
 
 #define RunInstance(x) delete new x
-struct brute1
+struct brute
 {
 	struct Trie
 	{
@@ -120,11 +106,110 @@ struct brute1
 		}
 	} trie;
 
-	brute1()
+	brute()
 	{
 		for (int i = 1; i <= n; i++)
 			trie.insert(i);
 		printOut(trie.DP());
+	}
+};
+struct work
+{
+	struct SAM
+	{
+		struct Node
+		{
+			int len;
+			int link;
+			int pos;
+			int ch[3];
+			Node() : len(), link(), ch() {}
+		} nodes[maxn * 4];
+		int size;
+		int last;
+		SAM() : size(), G() { nodes[last = size++].link = -1; }
+		void extend(int x, int pos)
+		{
+			int cur = size++;
+			nodes[cur].len = nodes[last].len + 1;
+			nodes[cur].pos = pos;
+			int p = last;
+			last = cur;
+
+			for (; ~p && !nodes[p].ch[x]; p = nodes[p].link)
+				nodes[p].ch[x] = cur;
+			if (!~p)
+			{
+				nodes[cur].link = 0;
+				return;
+			}
+			int q = nodes[p].ch[x];
+			if (nodes[p].len + 1 == nodes[q].len)
+			{
+				nodes[cur].link = q;
+				return;
+			}
+			int clone = size++;
+			nodes[clone] = nodes[q];
+			nodes[clone].pos = pos;
+			nodes[clone].len = nodes[p].len + 1;
+			nodes[cur].link = nodes[q].link = clone;
+			for (; ~p && nodes[p].ch[x] == q; p = nodes[p].link)
+				nodes[p].ch[x] = clone;
+		}
+
+		int G[maxn * 4][3];
+		void build()
+		{
+			for (int i = 1; i < size; i++)
+				G[nodes[i].link][str[nodes[i].pos + nodes[nodes[i].link].len]] = i;
+		}
+		LL DP(int node = 0, int depth = 0)
+		{
+			LL base = 0;
+			if (node)
+			{
+				int length = nodes[node].len - nodes[nodes[node].link].len;
+				if (depth >= n)
+					return sum[n] - sum[depth - length + 1];
+				else
+					base = sum[depth] - sum[depth - length + 1];
+			}
+			LL temp[3] = {};
+			for (int i = 0; i < 3; i++)
+				if (G[node][i])
+					temp[i] = DP(G[node][i], depth + nodes[G[node][i]].len - nodes[node].len);
+
+			LL ret = 0;
+			for (int i = 0; i < 3; i++)
+			{
+				LL min = LLONG_MAX;
+				for (int j = 0; j < 3; j++)
+				{
+					if (G[node][j])
+					{
+						int accum = 0;
+						switch (table[i][j])
+						{
+						case 1: accum = w[depth + 1]; break;
+						case 0: accum = d[depth + 1]; break;
+						case -1: accum = 0; break;
+						}
+						min = std::min(min, temp[j] + accum);
+					}
+				}
+				ret = std::max(ret, min);
+			}
+			return ret + base;
+		}
+	} sam;
+
+	work()
+	{
+		for (int i = n << 1; i; i--)
+			sam.extend(str[i], i);
+		sam.build();
+		printOut(sam.DP());
 	}
 };
 
@@ -150,6 +235,11 @@ void run()
 		w[i] = readIn();
 		d[i] = readIn();
 	}
+	for (int i = 1; i <= n; i++)
+		sum[i] = w[i];
+	for (int i = 2; i <= n; i++)
+		sum[i] += sum[i - 1];
+
 	if (checkSame())
 	{
 		LL ans = 0;
@@ -160,7 +250,9 @@ void run()
 	}
 
 	if (n <= 1000)
-		RunInstance(brute1);
+		RunInstance(brute);
+	else
+		RunInstance(work);
 }
 
 int main()
